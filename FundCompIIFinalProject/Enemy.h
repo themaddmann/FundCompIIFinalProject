fefb6b5c626e322897314102ea6ccf9d148b3a69
irishmann313 <bmann@nd.edu>
2015-03-23 23:11:09 -0400

    Lab 7/8
    Enemy base class for Brian Mann and Steven Mike's final project as of
    3/23/15

---
+++ FundCompIIFinalProject/Enemy.h
@@ -0,0 +1,34 @@
+// Base class for enemies
+// Brain Mann
+// 3/17/15
+// Enemy.h
+
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+class Enemy{
+    public:
+        Enemy();
+        virtual void setattack();
+        virtual void setrange();
+        virtual void setspeed();
+        virtual void setsight();
+        virtual int attack(int) == 0;
+        virtual void move();
+        virtual void pursue(double);
+        virtual void sound(double);
+    protected:
+        int attackdamage;
+        double attackrange;
+        int movementspeed;
+        double visionrange;
+}
