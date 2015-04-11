4e271aca5ee0cf08a725c82a3eecb188705aaa8d
irishmann313 <bmann@nd.edu>
2015-04-11 19:26:48 -0400

    Enemy2
    Adding the enemy header file and .cpp file

---
+++ FundCompIIFinalProject/Enemy2.h
@@ -0,0 +1,36 @@
+// Class for enemies
+// Brain Mann
+// 4/8/15
+// Enemy.h
+
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+class Enemy{
+    public:
+        Enemy();
+        int attack();
+        int pursue(int,int,int);
+        void sound();
+        int getenemyX();
+        int getenemyY();
+        void setenemyX(int);
+        void setenemyY(int);
+        char openDoor();
+    private:
+        int xposition;
+        int yposition;
+        int attackdamage;
+        int attackrange;
+        int movementspeed;
+        int visionrange;
+};
